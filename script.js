66f9ab8dcfac58b969c02f2be22b058f1614b86f
Gabriel Otalora <agotalora7@soy.sena.edu.co>
2025-06-16 21:17:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function cargarPreguntasDesdeFirebase(callback) {
         [todas[i], todas[j]] = [todas[j], todas[i]];
       }
       preguntas = todas.slice(0, 3);
-      setTimeout(callback, 100); //pequeño delay
+      setTimeout(callback, 10); //pequeño delay
     })
     .catch(error => {
       console.error("Error al cargar preguntas:", error);
